9da786be9249f687b47fc72efd88f7901c8152b6
Riley Fry <rileyf250@gmail.com>
2025-03-26 12:48:35 -0400

    All ports changed to 400 and runs

--- Lab2/node-express-mongodb-api/index.js
+++ Lab2/node-express-mongodb-api/index.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 // Middleware
 app.use(bodyParser.json());
